742bbd0b1f5ae4944a01fcefc68dc859810baf69
Qingzhe Tao <nanjingtqz@foxmail.com>
2025-09-01 17:00:55 +1000

    PlayerRankingComponent Rewarding system

---
+++ source/core/src/main/com/deco2800/game/components/player/PlayerRankingComponent.java
@@ -0,0 +1,19 @@
+package com.deco2800.game.components.player;
+
+public class PlayerRankingComponent {
+    public int stageCompleted = 0;
+    public int enemiesKilled = 0;
+    public int remainingHealth = 0;
+    public int towersLeft = 0;
+    public float timeElapsed = 0f;
+    public boolean noDamageClear = false;
+
+    public void reset() {
+        stageCompleted = 0;
+        enemiesKilled = 0;
+        remainingHealth = 0;
+        towersLeft = 0;
+        timeElapsed = 0f;
+        noDamageClear = false;
+    }
+}
---
+++ source/core/src/main/com/deco2800/game/components/rewards/Reward.java
@@ -0,0 +1,30 @@
+package com.deco2800.game.components.rewards;
+
+public class Reward {
+    private String name;
+    private String description;
+    private int gold;
+
+    public Reward(String name, String description, int gold) {
+        this.name = name;
+        this.description = description;
+        this.gold = gold;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    @Override
+    public String toString() {
+        return name + " - " + description + " (+" + gold + " gold)";
+    }
+}

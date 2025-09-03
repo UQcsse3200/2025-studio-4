7b78010dd0cfdcdddb145aad0ea65a2ed1c037bd
Braydon Wootton <braydonw98@gmail.com>
2025-09-03 19:33:33 +1000

    Renamed base_enemy.png to grunt_enemy.png throughout whole file

--- source/core/src/test/com/csse3200/game/entities/factories/BossEnemyFactoryTest.java
+++ source/core/src/test/com/csse3200/game/entities/factories/BossEnemyFactoryTest.java
@@ -28,7 +28,7 @@ public class BossEnemyFactoryTest {
         ServiceLocator.registerResourceService(resourceService);
         
         // Load assets needed for PlayerFactory
-        resourceService.loadTextures(new String[]{"images/box_boy_leaf.png", "images/base_enemy.png", "images/boss_enemy.png", "images/drone_enemy.png", "images/tank_enemy.png"});
+        resourceService.loadTextures(new String[]{"images/box_boy_leaf.png", "images/grunt_enemy.png", "images/boss_enemy.png", "images/drone_enemy.png", "images/tank_enemy.png"});
         resourceService.loadAll();
     }
 

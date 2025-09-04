356f620b4e1e788b7f621da1ffeba7e65ff5e63e
俞皓博 <yy36250pp@outlook.com>
2025-09-04 12:43:05 +1000

    rename spawnGhosts to spawnGrunts in ForestGameArea

--- source/core/src/main/com/csse3200/game/areas/ForestGameArea.java
+++ source/core/src/main/com/csse3200/game/areas/ForestGameArea.java
@@ -277,7 +277,7 @@ public class ForestGameArea extends GameArea {
     return null;
   }
 
-  private void spawnGhosts() {
+  private void spawnGrunts() {
 
     GridPoint2 minPos = new GridPoint2(0, 0);
     GridPoint2 maxPos = terrain.getMapBounds(0).sub(2, 2);

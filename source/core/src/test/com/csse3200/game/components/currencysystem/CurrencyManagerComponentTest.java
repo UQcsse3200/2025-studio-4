789298ed46aa4c7f607331830d87c3cb1b3a2ab9
Thanh Nhat Quang Mai <thanhnhatquang.mai@student.uq.edu.au>
2025-09-13 07:16:14 +1000

    Remove unused import

--- source/core/src/test/com/csse3200/game/components/currencysystem/CurrencyManagerComponentTest.java
+++ source/core/src/test/com/csse3200/game/components/currencysystem/CurrencyManagerComponentTest.java
@@ -13,7 +13,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.EnumMap;
-import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;

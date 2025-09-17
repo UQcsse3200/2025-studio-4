b4582276c4573a159814c1b134629a4265c557b2
Lex1377 <1395963675@qq.com>
2025-09-17 22:06:48 +1000

    add the test code

---
+++ source/core/src/test/com/csse3200/game/integration/AudioIntegrationTest.java
@@ -0,0 +1,177 @@
+package com.csse3200.game.integration;
+
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.csse3200.game.components.audio.BackgroundMusicComponent;
+import com.csse3200.game.components.audio.SoundEffectComponent;
+import com.csse3200.game.entities.Entity;
+import com.csse3200.game.extensions.GameExtension;
+import com.csse3200.game.files.UserSettings;
+import com.csse3200.game.services.AudioService;
+import com.csse3200.game.services.ResourceService;
+import com.csse3200.game.services.ServiceLocator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(GameExtension.class)
+class AudioIntegrationTest {
+
+    @Mock
+    private ResourceService mockResourceService;
+    
+    @Mock
+    private Music mockMusic;
+    
+    @Mock
+    private Sound mockSound;
+    
+    private AudioService audioService;
+    private Entity musicEntity;
+    private Entity soundEntity;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        ServiceLocator.registerResourceService(mockResourceService);
+        
+        when(mockResourceService.getAsset("bgm.mp3", Music.class)).thenReturn(mockMusic);
+        when(mockResourceService.getAsset("effect.ogg", Sound.class)).thenReturn(mockSound);
+        
+        audioService = new AudioService();
+        ServiceLocator.registerAudioService(audioService);
+        
+        musicEntity = new Entity();
+        musicEntity.addComponent(new BackgroundMusicComponent());
+        musicEntity.create();
+        
+        soundEntity = new Entity();
+        soundEntity.addComponent(new SoundEffectComponent());
+        soundEntity.create();
+    }
+
+    @Test
+    void shouldIntegrateBackgroundMusicFlow() {
+        audioService.registerMusic("bgm", "bgm.mp3");
+        
+        musicEntity.getEvents().trigger("playMusic", "bgm");
+        verify(mockMusic).setVolume(0.5f);
+        verify(mockMusic).setLooping(true);
+        verify(mockMusic).play();
+        
+        when(mockMusic.isPlaying()).thenReturn(true);
+        musicEntity.getEvents().trigger("pauseMusic");
+        verify(mockMusic).pause();
+        
+        when(mockMusic.isPlaying()).thenReturn(false);
+        musicEntity.getEvents().trigger("resumeMusic");
+        verify(mockMusic, times(2)).play();
+        
+        musicEntity.getEvents().trigger("stopMusic");
+        verify(mockMusic).stop();
+    }
+
+    @Test
+    void shouldIntegrateSoundEffectFlow() {
+        audioService.registerSound("effect", "effect.ogg");
+        
+        soundEntity.getEvents().trigger("playSound", "effect");
+        verify(mockSound).play(0.5f);
+        
+        soundEntity.getEvents().trigger("playSoundWithVolume", "effect", 0.8f);
+        verify(mockSound).play(0.8f);
+    }
+
+    @Test
+    void shouldHandleVolumeChanges() {
+        audioService.registerMusic("bgm", "bgm.mp3");
+        audioService.registerSound("effect", "effect.ogg");
+        
+        musicEntity.getEvents().trigger("playMusic", "bgm");
+        
+        audioService.setMusicVolume(0.8f);
+        verify(mockMusic).setVolume(0.8f);
+        
+        audioService.setSoundVolume(0.3f);
+        soundEntity.getEvents().trigger("playSound", "effect");
+        verify(mockSound).play(0.3f);
+    }
+
+    @Test
+    void shouldHandleSettingsIntegration() {
+        UserSettings.Settings settings = new UserSettings.Settings();
+        settings.musicVolume = 0.7f;
+        settings.soundVolume = 0.6f;
+        
+        audioService.updateSettings();
+        
+        audioService.registerMusic("bgm", "bgm.mp3");
+        musicEntity.getEvents().trigger("playMusic", "bgm");
+        
+        audioService.registerSound("effect", "effect.ogg");
+        soundEntity.getEvents().trigger("playSound", "effect");
+        
+        verify(mockMusic).setVolume(anyFloat());
+        verify(mockSound).play(anyFloat());
+    }
+
+    @Test
+    void shouldHandleMusicTransitions() {
+        Music mockMusic2 = mock(Music.class);
+        when(mockResourceService.getAsset("bgm2.mp3", Music.class)).thenReturn(mockMusic2);
+        
+        audioService.registerMusic("bgm1", "bgm.mp3");
+        audioService.registerMusic("bgm2", "bgm2.mp3");
+        
+        musicEntity.getEvents().trigger("playMusic", "bgm1");
+        verify(mockMusic).play();
+        
+        musicEntity.getEvents().trigger("changeMusic", "bgm2");
+        verify(mockMusic).stop();
+        verify(mockMusic2).play();
+    }
+
+    @Test
+    void shouldHandleMultipleEntitiesWithAudio() {
+        Entity anotherMusicEntity = new Entity();
+        anotherMusicEntity.addComponent(new BackgroundMusicComponent());
+        anotherMusicEntity.create();
+        
+        Entity anotherSoundEntity = new Entity();
+        anotherSoundEntity.addComponent(new SoundEffectComponent());
+        anotherSoundEntity.create();
+        
+        audioService.registerMusic("bgm", "bgm.mp3");
+        audioService.registerSound("effect", "effect.ogg");
+        
+        musicEntity.getEvents().trigger("playMusic", "bgm");
+        anotherMusicEntity.getEvents().trigger("playMusic", "bgm");
+        
+        soundEntity.getEvents().trigger("playSound", "effect");
+        anotherSoundEntity.getEvents().trigger("playSound", "effect");
+        
+        verify(mockMusic, times(2)).play();
+        verify(mockSound, times(2)).play(anyFloat());
+    }
+
+    @Test
+    void shouldHandleDisabledAudio() {
+        audioService.registerMusic("bgm", "bgm.mp3");
+        audioService.registerSound("effect", "effect.ogg");
+        
+        audioService.setMusicEnabled(false);
+        audioService.setSoundEnabled(false);
+        
+        musicEntity.getEvents().trigger("playMusic", "bgm");
+        soundEntity.getEvents().trigger("playSound", "effect");
+        
+        verify(mockMusic, never()).play();
+        verify(mockSound, never()).play(anyFloat());
+    }
+}
---
+++ source/core/src/test/com/csse3200/game/services/AudioServiceTest.java
@@ -0,0 +1,212 @@
+package com.csse3200.game.services;
+
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.csse3200.game.extensions.GameExtension;
+import com.csse3200.game.files.UserSettings;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(GameExtension.class)
+class AudioServiceTest {
+
+    @Mock
+    private ResourceService resourceService;
+    
+    @Mock
+    private Music mockMusic;
+    
+    @Mock
+    private Sound mockSound;
+    
+    private AudioService audioService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        ServiceLocator.registerResourceService(resourceService);
+        audioService = new AudioService();
+    }
+
+    @Test
+    void shouldRegisterMusic() {
+        when(resourceService.getAsset("test.mp3", Music.class)).thenReturn(mockMusic);
+        
+        audioService.registerMusic("testMusic", "test.mp3");
+        
+        verify(resourceService).getAsset("test.mp3", Music.class);
+    }
+
+    @Test
+    void shouldRegisterSound() {
+        when(resourceService.getAsset("test.ogg", Sound.class)).thenReturn(mockSound);
+        
+        audioService.registerSound("testSound", "test.ogg");
+        
+        verify(resourceService).getAsset("test.ogg", Sound.class);
+    }
+
+    @Test
+    void shouldPlayMusic() {
+        when(resourceService.getAsset("test.mp3", Music.class)).thenReturn(mockMusic);
+        audioService.registerMusic("testMusic", "test.mp3");
+        
+        audioService.playMusic("testMusic", true);
+        
+        verify(mockMusic).setVolume(0.5f);
+        verify(mockMusic).setLooping(true);
+        verify(mockMusic).play();
+    }
+
+    @Test
+    void shouldNotPlayMusicWhenDisabled() {
+        when(resourceService.getAsset("test.mp3", Music.class)).thenReturn(mockMusic);
+        audioService.registerMusic("testMusic", "test.mp3");
+        audioService.setMusicEnabled(false);
+        
+        audioService.playMusic("testMusic", true);
+        
+        verify(mockMusic, never()).play();
+    }
+
+    @Test
+    void shouldPlaySound() {
+        when(resourceService.getAsset("test.ogg", Sound.class)).thenReturn(mockSound);
+        audioService.registerSound("testSound", "test.ogg");
+        
+        audioService.playSound("testSound");
+        
+        verify(mockSound).play(0.5f);
+    }
+
+    @Test
+    void shouldNotPlaySoundWhenDisabled() {
+        when(resourceService.getAsset("test.ogg", Sound.class)).thenReturn(mockSound);
+        audioService.registerSound("testSound", "test.ogg");
+        audioService.setSoundEnabled(false);
+        
+        audioService.playSound("testSound");
+        
+        verify(mockSound, never()).play(anyFloat());
+    }
+
+    @Test
+    void shouldStopMusic() {
+        when(resourceService.getAsset("test.mp3", Music.class)).thenReturn(mockMusic);
+        audioService.registerMusic("testMusic", "test.mp3");
+        audioService.playMusic("testMusic", true);
+        
+        audioService.stopMusic();
+        
+        verify(mockMusic).stop();
+    }
+
+    @Test
+    void shouldPauseMusic() {
+        when(resourceService.getAsset("test.mp3", Music.class)).thenReturn(mockMusic);
+        audioService.registerMusic("testMusic", "test.mp3");
+        audioService.playMusic("testMusic", true);
+        when(mockMusic.isPlaying()).thenReturn(true);
+        
+        audioService.pauseMusic();
+        
+        verify(mockMusic).pause();
+    }
+
+    @Test
+    void shouldResumeMusic() {
+        when(resourceService.getAsset("test.mp3", Music.class)).thenReturn(mockMusic);
+        audioService.registerMusic("testMusic", "test.mp3");
+        audioService.playMusic("testMusic", true);
+        when(mockMusic.isPlaying()).thenReturn(false);
+        
+        audioService.resumeMusic();
+        
+        verify(mockMusic, times(2)).play();
+    }
+
+    @Test
+    void shouldSetMusicVolume() {
+        when(resourceService.getAsset("test.mp3", Music.class)).thenReturn(mockMusic);
+        audioService.registerMusic("testMusic", "test.mp3");
+        audioService.playMusic("testMusic", true);
+        
+        audioService.setMusicVolume(0.8f);
+        
+        verify(mockMusic).setVolume(0.8f);
+        assertEquals(0.8f, audioService.getMusicVolume());
+    }
+
+    @Test
+    void shouldClampMusicVolume() {
+        audioService.setMusicVolume(1.5f);
+        assertEquals(1.0f, audioService.getMusicVolume());
+        
+        audioService.setMusicVolume(-0.5f);
+        assertEquals(0.0f, audioService.getMusicVolume());
+    }
+
+    @Test
+    void shouldSetSoundVolume() {
+        audioService.setSoundVolume(0.7f);
+        assertEquals(0.7f, audioService.getSoundVolume());
+    }
+
+    @Test
+    void shouldClampSoundVolume() {
+        audioService.setSoundVolume(1.5f);
+        assertEquals(1.0f, audioService.getSoundVolume());
+        
+        audioService.setSoundVolume(-0.5f);
+        assertEquals(0.0f, audioService.getSoundVolume());
+    }
+
+    @Test
+    void shouldToggleMusicEnabled() {
+        when(resourceService.getAsset("test.mp3", Music.class)).thenReturn(mockMusic);
+        audioService.registerMusic("testMusic", "test.mp3");
+        audioService.playMusic("testMusic", true);
+        
+        assertTrue(audioService.isMusicEnabled());
+        
+        audioService.setMusicEnabled(false);
+        assertFalse(audioService.isMusicEnabled());
+        verify(mockMusic).pause();
+        
+        audioService.setMusicEnabled(true);
+        assertTrue(audioService.isMusicEnabled());
+        verify(mockMusic, times(2)).play();
+    }
+
+    @Test
+    void shouldToggleSoundEnabled() {
+        assertTrue(audioService.isSoundEnabled());
+        
+        audioService.setSoundEnabled(false);
+        assertFalse(audioService.isSoundEnabled());
+    }
+
+    @Test
+    void shouldStopPreviousMusicWhenPlayingNew() {
+        Music mockMusic2 = mock(Music.class);
+        when(resourceService.getAsset("test1.mp3", Music.class)).thenReturn(mockMusic);
+        when(resourceService.getAsset("test2.mp3", Music.class)).thenReturn(mockMusic2);
+        
+        audioService.registerMusic("music1", "test1.mp3");
+        audioService.registerMusic("music2", "test2.mp3");
+        
+        audioService.playMusic("music1", true);
+        audioService.playMusic("music2", true);
+        
+        verify(mockMusic).stop();
+        verify(mockMusic2).play();
+    }
+}
